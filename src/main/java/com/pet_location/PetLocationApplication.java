f187940182362257f81058d26ade1f98acc1f92a
unknown <ddsp.pinheiro@gmail.com>
2025-03-21 15:04:19 -0300

    feat: add initial structure

---
+++ src/main/java/com/pet_location/PetLocationApplication.java
@@ -0,0 +1,13 @@
+package com.pet_location;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetLocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetLocationApplication.class, args);
+	}
+
+}
